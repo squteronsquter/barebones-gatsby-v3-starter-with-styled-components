6d75be696686bcad504e645ff7feeb614cdefd12
squteronsquter <squter@squter.pl>
2021-09-18 11:57:24 +0200

    Gatsby plugin image installed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 }
