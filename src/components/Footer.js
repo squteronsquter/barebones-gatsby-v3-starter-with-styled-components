8cc88ae293834d7607341d3cd77e2ea50f4fd47c
squteronsquter <squter@squter.pl>
2021-09-18 07:23:54 +0200

    Styles added for navigation, footer and home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,33 @@
 import React from "react"
+import styled from "styled-components"
 
 const Footer = () => {
   return (
-    <footer>
-      <p>
-        &copy; {new Date().getFullYear()} <span>SimplyRecipes</span>. Built with{" "}
-        <a href="https://www.gatsbyjs.com/">Gatsby</a>
-      </p>
-    </footer>
+    <Wrapper>
+      <footer>
+        <p>
+          &copy; {new Date().getFullYear()} <span>SimplyRecipes</span>. Built
+          with <a href="https://www.gatsbyjs.com/">Gatsby</a>
+        </p>
+      </footer>
+    </Wrapper>
   )
 }
 
 export default Footer
+
+const Wrapper = styled.section`
+  background-color: #ccc;
+  margin-top: 2em;
+  display: flex;
+  height: 4em;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  a {
+    color: #2d4a7d;
+  }
+  a:hover {
+    text-decoration: none;
+  }
+`
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,54 @@
 import React from "react"
+import styled from "styled-components"
 import { Link } from "gatsby"
 
 export default function Navbar() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <Wrapper>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </Wrapper>
     </>
   )
 }
+
+const Wrapper = styled.section`
+  font-size: 1.4em;
+  margin-bottom: 1em;
+  nav {
+    background-color: #ccc;
+    width: 100%;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  ul {
+    display: flex;
+    justify-content: start;
+    align-items: center;
+  }
+  nav ul li {
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
+    color: #2a2a2a;
+    padding: 1em 3em;
+  }
+  a:hover {
+    color: #2d4a7d;
+  }
+`
